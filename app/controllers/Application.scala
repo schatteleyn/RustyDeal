1e5eb9d04e1149314c65995a4262e311ea7e17f9
Simon Chatteleyn <kilik2049@gmail.com>
2013-10-13 15:01:27 +0200

    add some static pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,16 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def listen = Action {
+    Ok(views.html.listen)
+  }
   
+  def band = Action {
+    Ok(views.html.band)
+  }
+  
+  def links = Action {
+    Ok(views.html.links)
+  }
 }
